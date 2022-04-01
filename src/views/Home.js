d4b2ce928418d85b47bf750e2b4121b7efdea286
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-04-01 11:31:21 -0700

    email and password input text box is displayin gon page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,39 @@
 import React, { useState } from 'react';
-import { signInUser } from '../services/users';
+import { signInUser, signupUser } from '../services/users';
 export default function Home() {
-    const [email, setEmail]= useState('');
-    const [password, setPassword] = useState('');
-};
-return (
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [type, setType] = useState('sign-in');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (type === 'sign-in') {
+      const resp = await signInUser(email, password);
+    } else {
+      const resp = await signupUser(email, password);
+    }
+  };
+  return (
     <div>
-        Home
+      <h1>
+        <span className={type === 'sign-in' ? 'active' : ''} onClick={() => setType('sign-in')}>
+          Sign In
+        </span>
+        <span className={type === 'sign-up' ? 'active' : ''} nClick={() => setType('sign-up')}>
+          Sign Up
+        </span>
+      </h1>
+      <form className="auth" onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button className="">Enter</button>
+      </form>
     </div>
-);
+  );
 }
