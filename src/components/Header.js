f889e2a4f1ace91cb1b4cb4e7b97ace583b51dbe
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-04-04 22:52:20 -0700

    refactored header and passed in current user. log out button works correctly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,14 @@
 import React from 'react';
-import { logout } from '../services/users';
-import { NavLink } from 'react-router-dom';
 
-export default function Header({ currentUser, setCurrentUser }) {
-  const handleLogout = async () => {
-    await logout();
-    setCurrentUser(null);
-  };
+export default function Header({ currentUser, handleLogout }) {
   return (
-    <div className="header">
-      <ul>
-        <li>
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-        </li>
-
-        {!currentUser && (
-          <li>
-            <NavLink to="/auth">Sign In</NavLink>
-          </li>
-        )}
-        {currentUser && (
-          <li>
-            <NavLink exact to="/todos/:id/create">
-              Add Dog
-            </NavLink>
-          </li>
-        )}
-      </ul>
+    <header>
+      <h2>Todos List!</h2>
       {currentUser && (
-        <ul>
-          <li>{currentUser}</li>
-          <li className="link" onClick={handleLogout}>
-            Logout
-          </li>
-        </ul>
+        <div className="link" onClick={handleLogout}>
+          logout
+        </div>
       )}
-    </div>
+    </header>
   );
 }
